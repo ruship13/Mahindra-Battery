6541abd4c20a908be8c2afea3ca988c9bbe75b64
ruship13 <126866397+ruship13@users.noreply.github.com>
2026-06-04 06:17:36 +0000

    d

--- src/main/java/com/ats/mahindrabattery/controller/LoginController.java
+++ src/main/java/com/ats/mahindrabattery/controller/LoginController.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.Random;
 
 import javax.servlet.http.HttpSession;
-
+import org.springframework.web.bind.annotation.RequestBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
