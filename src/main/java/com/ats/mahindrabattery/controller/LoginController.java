a40a214722db83d8372f41cd1c44d034ed54f741
ruship13 <126866397+ruship13@users.noreply.github.com>
2026-06-04 06:14:53 +0000

    12

--- src/main/java/com/ats/mahindrabattery/controller/LoginController.java
+++ src/main/java/com/ats/mahindrabattery/controller/LoginController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ats.mahindrabattery.entity.LoginRequest;
 import com.ats.mahindrabattery.entity.MasterUserDetailsEntity;
 import com.ats.mahindrabattery.entity.OtpEntity;
 import com.ats.mahindrabattery.repository.MasterUserDetailsRepository;
